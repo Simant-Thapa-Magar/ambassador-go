865b76e2c4b021962f35ab66635360d4c7b41c07
Simant-Thapa-Magar <tmsimant96@gmail.com>
2022-08-29 15:09:59 +0545

    database init

--- main.go
+++ main.go
@@ -4,9 +4,17 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	_, err := gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambassadorDB"), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to open database connection")
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
