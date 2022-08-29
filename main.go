d85d95efbf1ba58e05d23c6caeed65f696898e4a
Simant-Thapa-Magar <tmsimant96@gmail.com>
2022-08-29 15:57:17 +0545

    database and model as package, auto migration

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
+	"ambassador/src/database"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 func main() {
-	_, err := gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambassadorDB"), &gorm.Config{})
-
-	if err != nil {
-		panic("Failed to open database connection")
-	}
-
+	database.Connect()
+	database.AutoMigrate()
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
---
+++ src/database/db.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	models "ambassador/src/model"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	var err error
+	DB, err = gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambassadorDB"), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to open database connection")
+	}
+}
+
+func AutoMigrate() {
+	DB.AutoMigrate(models.User{})
+}
